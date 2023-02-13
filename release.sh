f68c6fd5fdd0b17d7e6a0d3e470cbcad43f00c14
Michael Mosmann <michael@mosmann.de>
2023-02-13 07:45:19 +0100

    released

--- release.sh
+++ release.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
-hint() {
-	echo "*********************************************"
-	echo "* as mvn release increments just the number *"
-	echo "* in any version string make sure that you  *"
-	echo "* change the version                        *"
-	echo "* not from spring2x to spring2(x+1)         *"
-	echo "*********************************************"
-}
 
 ./mvnw clean install
 ./mvnw release:clean
-hint
 ./mvnw release:prepare -DskipTests -Darguments=-DskipTests
-hint
 ./mvnw release:perform -DskipTests -Darguments=-DskipTests
 
