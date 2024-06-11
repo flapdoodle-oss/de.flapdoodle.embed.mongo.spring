4665f4cb5dcb624911259ca6a61f5517e460ba87
Michael Mosmann <michael@mosmann.de>
2024-06-11 07:24:27 +0200

    startup timeout configurable

--- src/test/java/de/flapdoodle/embed/mongo/spring/autoconfigure/HowToTest.java
+++ src/test/java/de/flapdoodle/embed/mongo/spring/autoconfigure/HowToTest.java
@@ -22,10 +22,7 @@ package de.flapdoodle.embed.mongo.spring.autoconfigure;
 
 import de.flapdoodle.embed.mongo.spring.autoconfigure.customize.CustomizeMongodTest;
 import de.flapdoodle.embed.mongo.spring.autoconfigure.customize.LocalConfig;
-import de.flapdoodle.embed.mongo.spring.autoconfigure.simple.AutoConfigFirstIsolationTest;
-import de.flapdoodle.embed.mongo.spring.autoconfigure.simple.AutoConfigSecondIsolationTest;
-import de.flapdoodle.embed.mongo.spring.autoconfigure.simple.AutoConfigTest;
-import de.flapdoodle.embed.mongo.spring.autoconfigure.simple.CustomDatabaseDirTest;
+import de.flapdoodle.embed.mongo.spring.autoconfigure.simple.*;
 import de.flapdoodle.embed.mongo.spring.autoconfigure.transactions.PersonRepository;
 import de.flapdoodle.embed.mongo.spring.autoconfigure.transactions.PersonService;
 import de.flapdoodle.embed.mongo.spring.autoconfigure.transactions.TransactionalConfig;
@@ -50,6 +47,7 @@ public class HowToTest {
 		.sourceCodeOf("transaction.service", PersonService.class, Includes.WithoutImports, Includes.WithoutPackage, Includes.Trim)
 		.sourceCodeOf("transaction.repository", PersonRepository.class, Includes.WithoutImports, Includes.WithoutPackage, Includes.Trim)
 		.sourceCodeOf("customDatabaseDir", CustomDatabaseDirTest.class, Includes.WithoutImports, Includes.WithoutPackage, Includes.Trim)
+		.sourceCodeOf("customStartTimeout", CustomStartTimeoutTest.class, Includes.WithoutImports, Includes.WithoutPackage, Includes.Trim)
 		.sourceCodeOf("customizeMongod", CustomizeMongodTest.class, Includes.WithoutImports, Includes.WithoutPackage, Includes.Trim)
 		.sourceCodeOf("customizeMongod.config", LocalConfig.class, Includes.WithoutImports, Includes.WithoutPackage, Includes.Trim)
 		;
---
+++ src/test/java/de/flapdoodle/embed/mongo/spring/autoconfigure/simple/CustomStartTimeoutTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2011
+ *   Michael Mosmann <michael@mosmann.de>
+ *   Martin Jöhren <m.joehren@googlemail.com>
+ *
+ * with contributions from
+ * 	...
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.flapdoodle.embed.mongo.spring.autoconfigure.simple;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+
+@AutoConfigureDataMongo
+@SpringBootTest(
+	properties = {
+		"de.flapdoodle.mongodb.embedded.starttimeout=10000"
+	}
+)
+@EnableAutoConfiguration(
+	exclude = org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration.class
+)
+@DirtiesContext
+public class CustomStartTimeoutTest {
+
+	@Test
+	void example(@Autowired final MongoTemplate mongoTemplate) {
+		Assertions.assertThat(mongoTemplate.getDb()).isNotNull();
+	}
+}
